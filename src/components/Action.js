d33e33a4156038b9d1a4ab5701cbbab869e62ef0
Fktrctq <henrysaddler@gmail.com>
2018-06-30 17:08:47 +0400

    Add styles

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Action = props => (
   <div>
-    <button disabled={!props.hasOptions} onClick={props.handlePickOption}>
+    <button
+      className="big-button"
+      disabled={!props.hasOptions}
+      onClick={props.handlePickOption}
+    >
       what should I do?
     </button>
   </div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -3,12 +3,23 @@ import Option from "./Option";
 
 const Options = props => (
   <div>
-    <button onClick={props.handleDeleteOptions}>remove all</button>
-       {props.options.length === 0 && <p>please add options</p>}
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        remove all
+      </button>
+    </div>
+       {props.options.length === 0 && (
+      <p className="widget__message">please add options</p>
+    )}
      {" "}
     {props.options.map((option, i) => (
       <Option
         key={i}
+        count={i + 1}
         optionText={option}
         handleDeleteOption={props.handleDeleteOption}
       />
